65657e3f3bd843c216ecb8d66f5c6118f3ae1ffb
russellgoldenberg <russellgoldenberg@gmail.com>
2018-06-07 09:10:07 -0400

    doneish

---
+++ src/assets/prism/code.js
@@ -0,0 +1,13 @@
+enterView({
+	selector: stepSel.nodes(),
+	offset: 0.5,
+	enter: el => {
+		const index = +d3.select(el).attr('data-index');
+		updateChart(index);
+	},
+	exit: el => {
+		let index = +d3.select(el).attr('data-index');
+		index = Math.max(0, index - 1);
+		updateChart(index);
+	}
+})
